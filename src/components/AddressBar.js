5c0fa229755eae14bcdafd22aa40af22415ddf84
billiamzhou <billamzhou@163.com>
2017-01-18 11:31:10 +0800

    加入文件
    新增

---
+++ src/components/AddressBar.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhouhuang on 2017/1/17.
+ */
+import React from 'react';
+
+export default React.createClass({
+    render: function () {
+        return <div className="address">
+            请填写收货地址
+        </div>;
+    }
+});
---
+++ src/components/StatementsButton.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zhouhuang on 2017/1/17.
+ */
+import React from 'react';
+
+export default React.createClass({
+    render: function () {
+        return <div>
+            <Button>结算</Button>
+        </div>;
+    }
+});
